86457258b8571593aafdd9bc3631358ec4afc9df
lmocs <loganmoreno.cs@gmail.com>
2025-03-08 16:38:52 -0800

    Update `getCards` to properly GET all cards.

--- back_end/models/cardModel.js
+++ back_end/models/cardModel.js
@@ -2,8 +2,22 @@ const db = require('../config/db');
 
 const Card = {
 	getCards: async () => {
+		const sql = `SELECT 
+						c.id,
+						c.title,
+						c.description,
+						c.priority,
+						c.status,
+						u1.name AS author,
+						u2.name AS designee,
+						c.author_id,
+						c.designee_id
+						FROM cards c
+						LEFT JOIN users u1 ON c.author_id = u1.id
+						LEFT JOIN users u2 ON c.designee_id = u2.id`
+
 		try {
-			const results = await db.query('SELECT * FROM SelfHelp');
+			const results = await db.query(sql);
 			console.log('Results:', results);
 			return results;
 		} catch (err) {
