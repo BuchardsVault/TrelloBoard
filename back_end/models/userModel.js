528e2adc2004db535d2796b31ba62fa1d6f086ef
lmocs <loganmoreno.cs@gmail.com>
2025-03-08 20:25:58 -0800

    Implement `addUser` function.

--- back_end/models/userModel.js
+++ back_end/models/userModel.js
@@ -14,7 +14,18 @@ const User = {
 		}
 	},
 
-	addUser: async (userData) => { },
+	addUser: async (userData) => {
+		const sqlInsertNewUser = `INSERT INTO users (name, email) VALUES (?, ?)`;
+		const userValues = [userData.name, userData.email];
+
+		try {
+			const results = await db.query(sqlInsertNewUser, userValues);
+			return results;
+		} catch (err) {
+			console.error('Error in addUser:', err);
+			throw err;
+		}
+	},
 };
 
 module.exports = User;
