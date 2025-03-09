ecfc3b148ec81b33761029cfdc444d8b945e6458
lmocs <loganmoreno.cs@gmail.com>
2025-03-08 16:40:22 -0800

    Use new route for the card API.

--- back_end/server.js
+++ back_end/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const app = express();
+const cardRoutes = require('./routes/cardRoutes')
 
 const PORT = 3000; // Or your chosen port
 const HOST = '0.0.0.0'; // Accepts external connections
 
-app.get('/', (req, res) => {
-	res.send('This is the `cards` API');
-});
+app.use("/api/cards", cardRoutes);
 
 app.listen(PORT, HOST, () => {
 	console.log(`Server running on http://${HOST}:${PORT}`);
